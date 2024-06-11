2590febbed864ae3d34c499f2c143f7ab4b5f575
mansoor1123456 <164507941+mansoor1123456@users.noreply.github.com>
2024-06-12 02:03:17 +0500

    TO-DO-LIST

---
+++ Lops.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+let ramadanDays = 0;
+while ((ramadanDays <= 30)) {
+    console.log("fasting");
+    console.log("fivetimeprayers");
+}
+let i = 0; //intialize
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+export {};
---
+++ array.js
@@ -0,0 +1,9 @@
+#! /usr/bin/env node
+let fruitchaat = ["Banana, Apple, Orange"];
+console.log(fruitchaat);
+// 'push()
+fruitchaat.pop();
+console.log(fruitchaat);
+fruitchaat = fruitchaat.concat(["Melon", "KIWI"]);
+console.log(fruitchaat);
+export {};
